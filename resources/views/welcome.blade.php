dd2e147eea498eb5d718bb8a32dbabf6d3b45f70
Casper Bekmand <bekmand@caspers-mini.home>
2016-05-07 11:52:57 +0100

    Uploaded with laravel framework

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,256 @@
+<!DOCTYPE HTML>
+<!--
+    Read Only by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <head>
+        <title>Andelsforeningen Sønder Lindbjerg II</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+        <link type="text/css" rel="stylesheet" href="{!! asset('css/main.css') !!}" />
+        <link type="text/css" rel="stylesheet" href="{!! asset('css/font-awesome.min.css.css') !!}" />
+        <link type="text/css" rel="stylesheet" href="{!! asset('css/custom.css') !!}" />
+
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+
+
+    </head>
+    <body>
+
+        <!-- Header -->
+            <section id="header">
+                <header>
+                    <!--<span class="image avatar"><img src="images/avatar.jpg" alt="" /></span>-->
+                    <h1 id="logo"><a href="#">Sønder Lindbjerg II</a></h1>
+                    <p>Velkommen til vores hjemmeside.<br />
+                    Sønder Lindbjerg II er en andelsforening øst for Herning i Gjelleup. Andelsforeningen blev stiftet i 1992.</p>
+
+
+
+
+                </header>
+                <nav id="nav">
+                    <ul>
+            <li><a href="#one" class="active">Forside</a></li>
+                        <li><a href="#two">Billeder</a></li>
+                        <li><a href="#three">Bestyrelsen</a></li>
+                        <li><a href="#four">Generalt</a></li><!--Vedtægter, Generalforsamling, Publikationer, Årskalender -->
+                        <li><a href="#five">Boligere til salg</a></li>
+                        <li><a href="#six">Renovering</a></li>
+                    </ul>
+                </nav>
+                <footer>
+                    <ul class="icons">
+                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+                    </ul>
+                </footer>
+            </section>
+
+        <!-- Wrapper -->
+            <div id="wrapper">
+
+                <!-- Main -->
+                    <div id="main">
+
+                        <!-- One -->
+                        <!--
+
+                            Slider skal indsættes her
+                            Der skal laves et header billede, ser lige om vi skal flytte slideren
+                            Og hvad vi skal skrive her under i stedet for.
+
+                        -->
+                            <section id="one">
+                                <div class="container">
+                                    <header class="major">
+                                        <h2>Sønder Lindbjerg II</h2>
+                                        <p>Andelsforening</p>
+                                    </header>
+                                    <p>Foreningen består af 14 boliger, alle i to plan. Boligerne er på mellem 94m2 og 96m2
+                                        Boligerne er bygget i tre kæder der tilsammen danner en kreds omkring et fælles haveanlæg. Haven holdes i fællesskab. </p>
+                                </div>
+                            </section>
+
+                        <!-- Two Slider insert -->
+                            <section id="two">
+                                <div class="container">
+                                    <div id="slides">
+                                        <img src="images/1.JPG">
+                                        <img src="images/2.JPG">
+                                        <img src="images/3.JPG">
+                                        <img src="images/4.JPG">
+                                        <img src="images/5.JPG">
+                                        <a href="#" class="slidesjs-previous slidesjs-navigation icon fa-arrow-left"></a>
+                                        <a href="#" class="slidesjs-next slidesjs-navigation icon fa-arrow-right"></a>
+                                    </div>
+                                </div>
+                            </section>
+
+                        <!-- Three -->
+                            <section id="three">
+                                <div class="container">
+                                    <h3>Bestyrelsen</h3>
+                                    <p>Formand: Janni</br>
+                                    Næstformand: Gitte</br>
+                                    Sekretær: Britta</br>
+                                    Bestyrrelsesmedlemmer: Birgit og Jeanette</br>
+                                    1. Suppleant: Morten</br>
+                                    2. Suppleant: Jens</p>
+
+                                    <ul class="feature-icons">
+                                        <li class="fa-file-pdf-o"><a href="#">Bestyrelsesreferat</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2011-12-13-14-15</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2010</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2009</a></li>
+                                    </ul>
+                                    <!--<div class="features">
+                                        <article>
+                                            <a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
+                                            <div class="inner">
+                                                <h4>Possibly broke spacetime</h4>
+                                                <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+                                            </div>
+                                        </article>
+                                        <article>
+                                            <a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
+                                            <div class="inner">
+                                                <h4>Terraformed a small moon</h4>
+                                                <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+                                            </div>
+                                        </article>
+                                        <article>
+                                            <a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
+                                            <div class="inner">
+                                                <h4>Snapped dark matter in the wild</h4>
+                                                <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+                                            </div>
+                                        </article>
+                                    </div>-->
+                                </div>
+                            </section>
+
+                        <!-- Four -->
+                            <section id="four">
+                                <div class="container">
+                                    <h3>Generalt</h3>
+                                    <h4>Vedtægter</h4>
+                                    <p>Ved generalforsamlingen 2013, vedtog beboerne i Sønder Lindbjerg II ændringerne i vedtægterne,
+                                        så de nu er opdateret i forhold til de krav vi har til vedtægterne samt til standardvedtægterne i ABF.</p>
+                                    <ul class="feature-icons">
+                                        <li class="fa-file-pdf-o"><a href="#">Vedtægter</a></li>
+                                    </ul>
+                                    <h4>Generalforsamling</h4>
+                                    <ul class="feature-icons">
+                                        <li class="fa-file-pdf-o"><a href="#">28 april 2011</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">23 april 2012</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">18 april 2013</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">24 april 2014</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">23 april 2015</a></li>
+                                    </ul>
+                                    <h4>Årsrapport</h4>
+                                    <ul class="feature-icons">
+                                        <li class="fa-file-pdf-o"><a href="#">2008</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2009</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2010</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2011</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2012</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2013</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">2014</a></li>
+                                    </ul>
+                                    <h4>Publikationer</h4>
+                                    <ul class="feature-icons">
+                                       <li class="fa-file-pdf-o"><a href="#">Velkommen til Sønder Lindbjerg</a></li>
+                                       <li class="fa-file-pdf-o"><a href="#">Ordensreglement</a></li>
+                                       <li class="fa-file-pdf-o"><a href="#">Overdragelsesprocedure</a></li>
+                                    </ul>
+                                    <h4>Årskalender</h4>
+                                    <p>8.6.2015 Bestyrelsesmøde</p>
+                                    <ul class="feature-icons">
+                                        <li class="fa-file-pdf-o"><a href="#">Græsslåningsplan 2015</a></li>
+                                    </ul>
+                                </div>
+                            </section>
+                        <!-- Five -->
+                            <section id="five">
+                                <div class="container">
+                                    <h3>Boligere til Salg</h3>
+                                    <p>Ved generelforsamlingen den 23 april 2015, blev det enstemmigt vedtaget, at et andelsbevis i Sønder Lindbjerg II,  vil koste 500.000 kr</br>
+                                    Den månedelige husleje er på 3100 kroner + eget forbrug af vand/varme/el.</p>
+
+                                    <p>Sønder Lindbjerg 85 er til salg via Danbolig</p>
+
+                                    <ul class="feature-icons">
+                                        <li class="fa-file-pdf-o"><a href="#">Energimærkning</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">Nøgletal 2014</a></li>
+                                        <li class="fa-file-pdf-o"><a href="#">Nøgletal 1 2014</a></li>
+                                    </ul>
+
+                                    </div>
+                            </section>
+
+                            <!-- Six  Renovering-->
+                            <section id="six">
+                                <div class="container">
+                                    <h3>Renovering</h3>
+                                    <p>I 2011 og starten af 2012 stod der renovering på planen i Andelsforeningen Sønder lindbjerg II.<p>
+
+                                    <p>Alle døre og vinduer blev udskiftet til mere energirigtige løsninger.
+                                        "Tårnene" er blevet efterisoleret og skifferpladerne er skiftet ud.
+                                        Med disse tiltag ser vi alle frem til en bedre varmeøkonomi i andelene.</p>
+
+                                    <p>Vores haveanlæg og stakitter rundt om vores haver er også blevet renoveret,
+                                        så det hele fremstår nyt og moderne. Noget vi har fået meget ros for fra forbipasserende.
+                                        Vi håber, at den nye have indbyder til mere socialt samvær.</p>
+
+                                    <p>Indenfor har badeværelserne fået nye silikonefuger overalt,
+                                        der er monteret udsugning i brusenischen og toilettet er udskiftet til et lavskyldstoilet.</p>
+
+                                    <p>Vi glæder os alle til at kunne tilbringe nogle rolige, varme og lange somre i vores nye omgivelser.</p>
+
+                                    <ul class="feature-icons">
+                                        <li class="fa-file-pdf-o"><a href="#">Referat fra infomøde 16.12 - 2010</li>
+                                        <li class="fa-file-pdf-o"><a href="#">Plan over renovering</li>
+                                    </ul>
+                                </div>
+                            </section>
+
+                    </div>
+
+                <!-- Footer -->
+                    <section id="footer">
+                        <div class="container">
+                            <ul class="copyright">
+                                <li>&copy; Andelsboligforening Sønder Lindbjerg II</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                            </ul>
+                        </div>
+                    </section>
+
+            </div>
+
+        <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/jquery.scrollzer.min.js"></script>
+            <script src="assets/js/jquery.scrolly.min.js"></script>
+            <script src="assets/js/skel.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+            <script src="assets/js/main.js"></script>
+            <script src="assets/js/jquery.slides.min.js"></script>
+
+            <script>
+            $(function() {
+                $('#slides').slidesjs({
+                width: 940,
+                height: 450,
+                navigation: true,
+                });
+            });
+            </script>
+
+    </body>
+</html>
